d9dfb050abea0a1e2c3d79151b313abdf88bbafe
andreaselmi <andreaselmi90@gmail.com>
2021-05-15 16:43:14 +0200

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,37 @@
 import React, { useEffect, useState } from "react";
-import { ThemeProvider } from "@material-ui/core/styles";
-import darkTheme from "./config/theme";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import { Element } from "react-scroll";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Element } from "react-scroll";
+//material ui
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 
-import HomePage from "./views/HomePage";
-import Navbar from "./components/Navbar";
-import links from "./config/routes";
-import SearchPage from "./views/SearchPage";
-import ExplorePage from "./views/ExplorePage";
-import ProtectedRoute from "./components/common/ProtectedRoute";
-import { auth, firestore } from "./config/firebase";
+//store
 import { useAppDispatch } from "./store/hooks";
 import { setCurrentUser } from "./store/user";
 
+//auth
+import { auth, firestore } from "./config/firebase";
+import firebase from "firebase/app";
+
+//my components
 import Footer from "./components/Footer";
 import AccountPage from "./views/AccountPage";
+import HomePage from "./views/HomePage";
+import Navbar from "./components/Navbar";
+import SearchPage from "./views/SearchPage";
+import ExplorePage from "./views/ExplorePage";
+import ProtectedRoute from "./components/common/ProtectedRoute";
+
+//config
+import links from "./config/routes";
+import darkTheme from "./config/theme";
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
   const [initializing, setInitializing] = useState<boolean>(true);
   const dispatch = useAppDispatch();
 
-  const authStateChanged = async (user: any) => {
+  const authStateChanged = async (user: firebase.User | null) => {
     if (user) {
       let provider = user.providerData[0]?.providerId;
 
@@ -38,22 +46,34 @@ function App() {
           })
         );
       } else if (provider === "facebook.com") {
-        dispatch(
-          setCurrentUser({
-            fullName: user.displayName,
-            email: user.email,
-            userImg: "",
-            uid: user.uid,
-            provider,
+        const userRef = await firestore.collection("users").doc(user.uid);
+        userRef
+          .get()
+          .then((doc) => {
+            if (doc.exists) {
+              dispatch(
+                setCurrentUser({
+                  fullName: doc.data()?.fullName,
+                  email: doc.data()?.email,
+                  userImg: doc.data()?.userImg,
+                  uid: doc.data()?.uid,
+                  provider: doc.data()?.provider,
+                })
+              );
+            } else {
+              userRef.set({
+                provider,
+                email: user.email,
+                fullName: user.displayName,
+                uid: user.uid,
+                userImg: "",
+              });
+            }
           })
-        );
-        //TODO supportare img facebook
-        await firestore.collection("users").doc(user.uid).set({
-          provider,
-          email: user.email,
-          fullName: user.displayName,
-          uid: user.uid,
-        });
+          .catch((error) => {
+            //TODO toast per gestire l'errore
+            console.log("Error getting document:", error);
+          });
       } else {
         //TODO migliorare
         await firestore
