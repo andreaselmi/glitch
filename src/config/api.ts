54c1b0c151e8fbb909f4c0ab6889cc845636c8d4
andreaselmi <andreaselmi90@gmail.com>
2021-05-25 00:01:38 +0200

    added comment and fixed refresh token handler

--- src/config/api.ts
+++ src/config/api.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const client_id = process.env.REACT_APP_CLIENT_ID;
 const secret_client_id = process.env.REACT_APP_SECRET_ID;
 
+// The twitch API requires a time-limited access token to pass to the api calls.
+// The following functions generate a request to twitch and save the token in local storage
+
 const requestAccessToken = async () => {
   return await axios.post(
     `https://id.twitch.tv/oauth2/token?client_id=${client_id}&client_secret=${secret_client_id}&grant_type=client_credentials`
@@ -64,15 +67,20 @@ helix.interceptors.request.use(
   }
 );
 
+//the access token has a duration of 60 days, in case it becomes invalid before this time it is deleted and regenerated
 helix.interceptors.response.use(
   (response) => {
     return response;
   },
   (error) => {
-    if (error.response.status === 401) {
+    if (
+      error.response.status === 401 &&
+      error.response.data.message === "Invalid OAuth token"
+    ) {
       localStorage.removeItem("accessToken");
       setAccessToken();
       window.location.reload();
+      console.log(error.response);
     }
     return Promise.reject(error.response.data);
   }
